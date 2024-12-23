82576aa271f5da1e8aebe9982362ae2c22c9f967
ZaibiMed <mohammed.zaibi@outlook.com>
2024-12-23 15:35:10 +0100

    Add AsyncEnumerable extensions

---
+++ src/AsyncEnumerableApi/Infrastructure/AsyncEnumerable/AsyncEnumerableExtensions.cs
@@ -0,0 +1,71 @@
+namespace AsyncEnumerableApi.Infrastructure.AsyncEnumerable;
+
+public static class AsyncEnumerableExtensions
+{
+    public static async IAsyncEnumerable<T> WithDelay<T>(
+        this IAsyncEnumerable<T> source,
+        int delayMilliseconds,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        await foreach (var item in source.WithCancellation(cancellationToken))
+        {
+            yield return item;
+            if (delayMilliseconds > 0)
+            {
+                await Task.Delay(delayMilliseconds, cancellationToken);
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<T> WithFilter<T>(
+        this IAsyncEnumerable<T> source,
+        Func<T, bool> predicate,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        await foreach (var item in source.WithCancellation(cancellationToken))
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<T> WithBatchDelay<T>(
+        this IAsyncEnumerable<T> source,
+        int batchSize,
+        int delayMilliseconds,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        int count = 0;
+        await foreach (var item in source.WithCancellation(cancellationToken))
+        {
+            yield return item;
+            count++;
+
+            if (count % batchSize == 0 && delayMilliseconds > 0)
+            {
+                await Task.Delay(delayMilliseconds, cancellationToken);
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<T> WithProgress<T>(
+        this IAsyncEnumerable<T> source,
+        Action<int> progressAction,
+        int reportEvery = 100,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        int count = 0;
+        await foreach (var item in source.WithCancellation(cancellationToken))
+        {
+            yield return item;
+            count++;
+
+            if (count % reportEvery == 0)
+            {
+                progressAction(count);
+            }
+        }
+    }
+}
